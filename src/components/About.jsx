a1bcfc65f14ce147ea094ad6916fe3ef5a72e52a
Bryan Clay <76086029+Bryan-clay@users.noreply.github.com>
2024-02-25 19:21:27 -0800

    About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import Tilt from 'react-tilt'
+import React from 'react';
+import { Tilt } from "react-tilt";
 import { motion } from 'framer-motion';
-
+import { SectionWrapper } from '../hoc';
 import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
@@ -64,4 +64,4 @@ const About = () => {
   );
 }
 
-export default About
+export default SectionWrapper(About, "about");
