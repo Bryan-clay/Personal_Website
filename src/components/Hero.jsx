0dbc7708371371ea2a6383750e1bff704d4c3976
Bryan Clay <76086029+Bryan-clay@users.noreply.github.com>
2024-03-03 17:20:01 -0800

    trying to figure out why motion is not rendering

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,13 @@
 import { motion } from "framer-motion";
 import {styles} from '../styles';
 import {ComputersCanvas} from './canvas';
-import React from "react";
+
 
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx auto flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#9153ff]" />
@@ -25,26 +25,24 @@ const Hero = () => {
             {/* <br className="sm:block hidden" /> */}
           </p>
         </div>
-        </div>
-        <ComputersCanvas />
+      </div>
+      <ComputersCanvas />
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className=" w-[35]px h-[64]px rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.div 
-            animate={{
-              y: [0, 10, 0]
-            }}
-            transition={{
-              duration: 1.5,
-              repeat: Infinity,
-              repeatType: "loop"
-            }}
-            className="w-1 h-2 rounded-full bg-secondary mb-1"
-              
-              />
+            <motion.div
+              animate={{
+                y: [0, 10, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-1 h-2 rounded-full bg-secondary mb-1"
+            />
           </div>
         </a>
-
       </div>
     </section>
   );
